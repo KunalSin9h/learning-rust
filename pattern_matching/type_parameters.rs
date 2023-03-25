e844d15bb35971967eeae076a014f28a39d94069
Kunal Singh <kunalsin9h@gmail.com>
2023-03-25 23:38:41 +0530

    add vectors

--- pattern_matching/type_parameters.rs
+++ pattern_matching/type_parameters.rs
@@ -1,2 +1,41 @@
+/*
+ * Type Parameters<T>
+ */
+
+enum Option<T>{
+    None,
+    Some(T),
+}
+
+enum Result<O, E> {
+    Ok(O),
+    Err(E),
+}
+
 fn main(){
+    let none: Option<char> = Option::None;
+    let some: Option<char> = Option::Some('a');
+
+    match none {
+        Option::None => println!("This is none"),
+        Option::Some(x) => println!("The value is {}", x), 
+    }
+
+    match some {
+        Option::None => println!("This is none"),
+        Option::Some(x) => println!("The value is {}", x), 
+    }
+
+    let ok: Result<u64, &str> = Result::Ok(10);
+    let err: Result<u64, &str> = Result::Err("Error Reported");
+
+    match ok {
+        Result::Ok(o) => println!("The result value is {}", o),
+        Result::Err(err) => println!("The error is {}", err),
+    }
+
+    match err {
+        Result::Ok(o) => println!("The result value is {}", o),
+        Result::Err(err) => println!("The error is {}", err),
+    }
 }
---
+++ vectors/vector.rs
@@ -0,0 +1,23 @@
+/*
+ */
+
+fn main(){
+
+    let mut years: Vec<i32> = vec![2002, 2003, 2004]; // macro called with []
+
+    years.push(2010);
+    years.push(2020);
+
+    println!("Number of years in years is {}", years.len());
+
+    for year in years {
+        println!("{}", year);
+    }
+
+    let arr: [i32; 3] = [-100, 0, 100];
+
+    for a in arr {
+        println!("{}", a);
+    }
+
+}
