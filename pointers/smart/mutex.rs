c9c11de2148e16e723980f1babbc790db6eb71ae
Kunal Singh <kunalsin9h@gmail.com>
2023-05-17 16:07:35 +0530

    made async

--- pointers/smart/mutex.rs
+++ pointers/smart/mutex.rs
@@ -1,18 +1,22 @@
 use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
 
 fn main(){
     let x = Arc::new(Mutex::new(0));
+    let mut tasks = Vec::new();
 
     for _ in 0..1000 {
         let y = x.clone();
         let ht = thread::spawn(move || {
-            let mut ym = y.lock().unwrap(); // i am going to acquire, means no thread can use it
+            let mut ym = y.lock().unwrap(); 
             *ym = *ym + 1;                                            
         });
-        let _ = ht.join();
+        tasks.push(ht);
     }
-    thread::sleep(Duration::from_secs(1));
-    println!("{:?}", x);
+
+    for t in tasks {
+        let _ = t.join();
+    }
+
+    println!("{:?}", *x.lock().unwrap());
 }
